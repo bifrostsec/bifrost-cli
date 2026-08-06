16cf7bd963ff732200c16b7797cacbbaf5e1067d
Alexander Singh <45212893+alexanderbsingh@users.noreply.github.com>
2026-08-06 14:09:00 +0200

    refactor: deprecate git auto-detect in favor of explicit repo path configuration (#14)
    * feat: replace git auto-detect with repo path
    
    * feat: deprecate git auto-detect in favor of explicit repo path configuration
    
    * refactor: use constant for git auto-detect environment variable
    
    * refactor: use constant for git auto-detect environment variable
    
    * refactor: use constant for git repo path environment variable
    
    * refactor: mark git-auto-detect as deprecated and update related handling logic
    
    * refactor: improve visibility of git-auto-detect deprecation in help text and remove redundant logic
    
    * refactor: simplify and rename deprecated git-auto-detect handling logic
    
    * fix: merge issues

--- internal/bifrost/cli.go
+++ internal/bifrost/cli.go
@@ -39,6 +39,10 @@ func CLI(version, gitCommit string, args []string) int {
 		printUsage(os.Stderr, fl)
 		return 2
 	}
+	if isDeprecatedGitAutoDetectSet(fl.FlagSet, &options) {
+		_, _ = fmt.Fprint(os.Stderr, gitAutoDetectDeprecationWarning)
+	}
+
 	err = ValidateBaseOptions(fl.FlagSet, &options)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Error: %v\n\n", err)
